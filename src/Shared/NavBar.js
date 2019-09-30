1220e0036d24879d6f416f0dd8dd2608bb61ba1b
Frederik hjorth <s175109@student.dtu.dk>
2019-09-30 17:40:10 +0200

    responsive navbar tilføjet

--- src/Shared/NavBar.js
+++ src/Shared/NavBar.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+
+
 
 
 class NavBar extends Component {
     render() {
         return(
-            <div className="navbar bg-light">
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+                <Navbar.Brand href="#home">Freelancer</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="#/market">Market</Nav.Link>
+                        <Nav.Link href="#/login">Log in</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        )
+    }
+}
+
+export default NavBar;
+
+
+/*
+<div className="navbar bg-light">
                 <div className="navbar-brand">Freelancer</div>
                 <div className="nav mr-auto">
                     <div className="nav-link" >Home</div>
@@ -12,8 +35,4 @@ class NavBar extends Component {
                     <div className="nav-link" >Pricing</div>
                 </div>
             </div>
-        )
-    }
-}
-
-export default NavBar;
+            */
