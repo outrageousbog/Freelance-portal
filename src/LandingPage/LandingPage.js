859288b9f81904a4b78c8682ee780639c1acac7f
Frederik hjorth <s175109@student.dtu.dk>
2019-09-30 20:10:11 +0200

    overskrift samt søgefelt tilføjet

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,10 +1,42 @@
 import React from "react";
+import "./LandingPage.css"
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
 
 
 function LandingPage() {
     return (
         <div>
-            <h1>Landing Page!</h1>
+            <Container>
+                <Row>
+                    <Col sm={12} lg={5}>
+                    <div className="Headline">
+                        <h1>Find nemt freelance jobs for studerende</h1>
+                    </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col sm={12} md={10} xl={12}>
+                    <div className="searchBar">
+                        <InputGroup className="mb-5">
+                            <FormControl
+                                placeholder="Job title eller søge ord"
+                                aria-label="Recipient's username"
+                                aria-describedby="basic-addon2"
+                            />
+                            <InputGroup.Append>
+                                <Button variant="outline-primary">Søg</Button>
+                            </InputGroup.Append>
+                        </InputGroup>
+                    </div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
