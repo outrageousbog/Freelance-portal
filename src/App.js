a3549c3eb24c440c28097799dec9ca58be3a699a
Frederik hjorth <s175109@student.dtu.dk>
2019-10-01 11:05:46 +0200

    footer tilføjet

--- src/App.js
+++ src/App.js
@@ -6,26 +6,32 @@ import MarketPage from "./MarketPage/MarketPage";
 import LoginPage from "./Login/LoginPage";
 import PageNotFound from "./InvalidPage/PageNotFound";
 import NavBar from "./Shared/NavBar"
+import Footer from "./Shared/Footer";
 
 
 function App() {
-  return (
-    <div className="App">
-        <React.Fragment>
-            <NavBar/>
-        </React.Fragment>
-      <HashRouter>
-          <div>
-              <Switch>
-                  <Route path={"/market"} component={MarketPage}/>
-                  <Route path={"/login"} component={LoginPage}/>
-                  <Route exact path={"/"} component={LandingPage}/>
-                  <Route component={PageNotFound}/>
-              </Switch>
-          </div>
-      </HashRouter>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="site-content">
+                <React.Fragment>
+                    <NavBar/>
+                </React.Fragment>
+                <HashRouter>
+                    <div>
+                        <Switch>
+                            <Route path={"/market"} component={MarketPage}/>
+                            <Route path={"/login"} component={LoginPage}/>
+                            <Route exact path={"/"} component={LandingPage}/>
+                            <Route component={PageNotFound}/>
+                        </Switch>
+                    </div>
+                </HashRouter>
+            </div>
+            <React.Fragment>
+                <Footer/>
+            </React.Fragment>
+        </div>
+    );
 }
 
 export default App;
