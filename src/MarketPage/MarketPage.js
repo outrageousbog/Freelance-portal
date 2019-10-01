3822fe9df0103950b083b425b3faaf247c48b6ee
UltimateDadBod <emilvinkel@gmail.com>
2019-10-01 10:10:43 +0200

    Card

--- src/MarketPage/MarketPage.js
+++ src/MarketPage/MarketPage.js
@@ -9,76 +9,77 @@ import Button from "react-bootstrap/Button";
 
 function MarketPage() {
     return (
-        <div>
-            <Container>
 
-                <Row>
-                    <Col sm={12} md={10} xl={12} className="search" class="mx-auto">
+        <Container fluid  sm={12} md={12} xl={12}>
+            <Row sm={12} md={12} xl={12}>
+            <Col  xl={3} >
+
+            </Col>
+            <Col xl={9} className="cards">
+                <Row sm={12} md={12} xl={12}>
+                    <Col className="search">
 
                         <InputGroup className="mb-5">
                             <FormControl
-                                placeholder="Job titel eller søge ord"
-                            />
+                                placeholder="Job titel eller søge ord"/>
                             <InputGroup.Append>
                                 <Button variant="outline-primary">Søg</Button>
                             </InputGroup.Append>
                         </InputGroup>
-
                     </Col>
                 </Row>
 
-                <Row sm={12} md={10} xl={12} class="mx-auto">
-                    <Col>
-                        <CardDeck>
-                            <Card>
-                                <Card.Img variant="top"
-                                          src="https://cdn.swcdn.net/creative/images/dnsStuff/29072016/computer_screen.jpg"/>
-                                <Card.Body>
-                                    <Card.Title>Søger IT-Studerende</Card.Title>
-                                    <Card.Text>
-                                        Har du lyst til nye udfordringer ? Klik ind og læs mere om danmarks bedste
-                                        studiejob.
-                                    </Card.Text>
-                                </Card.Body>
-                                <Card.Footer>
-                                    <small className="text-muted">Opdateret for 3 min siden</small>
-                                </Card.Footer>
-                            </Card>
-                            <Card>
-                                <Card.Img variant="top"
-                                          src="https://cdn.swcdn.net/creative/images/dnsStuff/29072016/computer_screen.jpg"/>
-                                <Card.Body>
-                                    <Card.Title>Søger IT-Studerende</Card.Title>
-                                    <Card.Text>
-                                        Har du lyst til nye udfordringer ? Klik ind og læs mere om danmarks bedste
-                                        studiejob.
-                                    </Card.Text>
-                                </Card.Body>
-                                <Card.Footer>
-                                    <small className="text-muted">Opdateret for 3 min siden</small>
-                                </Card.Footer>
-                            </Card>
-                            <Card>
-                                <Card.Img variant="top"
-                                          src="https://cdn.swcdn.net/creative/images/dnsStuff/29072016/computer_screen.jpg"/>
-                                <Card.Body>
-                                    <Card.Title>Søger IT-Studerende</Card.Title>
-                                    <Card.Text>
-                                        Har du lyst til nye udfordringer ? Klik ind og læs mere om danmarks bedste
-                                        studiejob.
-                                    </Card.Text>
-                                </Card.Body>
-                                <Card.Footer>
-                                    <small className="text-muted">Opdateret for 3 min siden</small>
-                                </Card.Footer>
-                            </Card>
-                        </CardDeck>
-                    </Col>
+                <Row>
+                    <CardDeck>
+                        <Card>
+                            <Card.Img variant="top"
+                                      src="https://cdn.swcdn.net/creative/images/dnsStuff/29072016/computer_screen.jpg"/>
+                            <Card.Body>
+                                <Card.Title>Søger IT-Studerende</Card.Title>
+                                <Card.Text>
+                                    Har du lyst til nye udfordringer ? Klik ind og læs mere om danmarks bedste
+                                    studiejob.
+                                </Card.Text>
+                            </Card.Body>
+                            <Card.Footer>
+                                <small className="text-muted">Opdateret for 3 min siden</small>
+                            </Card.Footer>
+                        </Card>
+                        <Card>
+                            <Card.Img variant="top"
+                                      src="https://cdn.swcdn.net/creative/images/dnsStuff/29072016/computer_screen.jpg"/>
+                            <Card.Body>
+                                <Card.Title>Søger IT-Studerende</Card.Title>
+                                <Card.Text>
+                                    Har du lyst til nye udfordringer ? Klik ind og læs mere om danmarks bedste
+                                    studiejob.
+                                </Card.Text>
+                            </Card.Body>
+                            <Card.Footer>
+                                <small className="text-muted">Opdateret for 3 min siden</small>
+                            </Card.Footer>
+                        </Card>
+                        <Card>
+                            <Card.Img variant="top"
+                                      src="https://cdn.swcdn.net/creative/images/dnsStuff/29072016/computer_screen.jpg"/>
+                            <Card.Body>
+                                <Card.Title>Søger IT-Studerende</Card.Title>
+                                <Card.Text>
+                                    Har du lyst til nye udfordringer ? Klik ind og læs mere om danmarks bedste
+                                    studiejob.
+                                </Card.Text>
+                            </Card.Body>
+                            <Card.Footer>
+                                <small className="text-muted">Opdateret for 3 min siden</small>
+                            </Card.Footer>
+                        </Card>
+                    </CardDeck>
+
                 </Row>
+            </Col>
+            </Row>
+        </Container>
 
-
-            </Container>
-        </div>
 
     )
 }
